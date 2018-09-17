7b90d276df24bb442f7957ff7f454f83a2616e5e
Olivier Niyomugabo <nivierlos@gmail.com>
2018-09-17 13:32:32 -0700

    generating student model and corresponding controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'students/new'
+  get 'students/create'
+  get 'students/edit'
+  get 'students/update'
+  get 'students/index'
+  get 'students/show'
+  get 'students/destroy'
+  root 'students#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20180917202722_create_students.rb
@@ -0,0 +1,24 @@
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.integer :reg_number
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :telephone
+      t.string :sex
+      t.string :department
+      t.integer :level
+      t.string :academic_year
+      t.string :sponsor
+      t.string :password
+      t.boolean :is_dean_approve
+      t.boolean :is_hod_approve
+      t.boolean :is_warden_approve
+      t.boolean :is_librarian_approve
+      t.boolean :is_finance_approve
+
+      t.timestamps
+    end
+  end
+end
